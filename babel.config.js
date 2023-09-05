e0ac2d4c4ce4c4ceb0309f25a9ee3123cab736b8
pepesoup <jdunfalk@gmail.com>
2023-09-05 02:31:26 +0200

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,36 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = function (api) {
+    api.cache(true)
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+                    root: ['.'],
+                    alias: {
+                        '@src': './src',
+                        '@components': './src/components',
+                    },
+                },
+            ],
+            'react-native-reanimated/plugin',
+            'expo-router/babel',
+            [
+                'module:react-native-dotenv',
+                {
+                    envName: 'APP_ENV',
+                    moduleName: '@env',
+                    path: '.env',
+                    blocklist: null,
+                    allowlist: null,
+                    blacklist: null, // DEPRECATED
+                    whitelist: null, // DEPRECATED
+                    safe: false,
+                    allowUndefined: false,
+                    verbose: false,
+                },
+            ],
+        ],
+    }
+}
---
+++ src/app/commonUi/index.tsx
@@ -0,0 +1,14 @@
+import { ButtonCmn, ScreenCmn } from "@components/commonUi";
+import React from "react";
+import {
+    MD3Theme,
+    MD3DarkTheme as PaperDarkTheme,
+    MD3LightTheme as PaperLightTheme,
+} from 'react-native-paper'
+
+
+export default function CommonUi() {
+    return (<ScreenCmn theme={PaperDarkTheme}>
+        <ButtonCmn title="ButtonCmn" />
+    </ScreenCmn>)
+}
